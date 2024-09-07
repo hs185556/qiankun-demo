e678ed5d130c3f0fc51ab756ed69689ab003f3f6
hs185556 <410262593@qq.com>
2024-09-07 16:15:42 +0800

    路由前缀

--- report/vue.config.js
+++ report/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: '/report',
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
