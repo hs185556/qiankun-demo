6a8a1e6fbe61809e0946be4fbab65f9429c02d78
hs185556 <410262593@qq.com>
2024-09-29 11:03:21 +0800

    路由重定向

--- report/src/router/index.js
+++ report/src/router/index.js
@@ -2,22 +2,32 @@ import HomeView from "../views/HomeView.vue";
 
 export const routes = [
   {
-    path: "/login",
-    name: "login",
-    component: () => import("../views/LoginView.vue"),
-  },
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "",
+    name: "Report",
+    children: [
+      {
+        path: "",
+        redirect: "about",
+      },
+      {
+        path: "login",
+        name: "login",
+        component: () => import("../views/LoginView.vue"),
+      },
+      {
+        path: "home",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "about",
+        name: "about",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      },
+    ],
   },
 ];
